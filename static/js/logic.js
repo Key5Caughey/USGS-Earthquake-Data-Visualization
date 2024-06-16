8d6776702560b18aabec629c58aa920e5a2c6860
Key <key5caughey@gmail.com>
2024-06-16 16:42:32 -0400

    adding more final changes

--- static/js/logic.js
+++ static/js/logic.js
@@ -16,10 +16,6 @@ let myMap = L.map("map", {
 });
 
 // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-// L.control.layers(baseMaps, overlayMaps, {
-//     collapsed: false
-//   }).addTo(map);
-
 
 basemap.addTo(myMap)
 let tectonicPlates = new L.LayerGroup();
